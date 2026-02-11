5183635b104a747625adaffb77249cbbdfc1e0d6
Luc Brinkman <75385930+lucbrinkman@users.noreply.github.com>
2026-02-11 10:56:54 +0000

    refactor(content-processor): deduplicate levenshtein function

--- content_processor/src/parser/wikilink.ts
+++ content_processor/src/parser/wikilink.ts
@@ -1,5 +1,6 @@
 // src/parser/wikilink.ts
 import { join, dirname, normalize } from 'path';
+import { levenshtein } from '../validator/field-typos.js';
 
 export interface WikilinkParts {
   path: string;
@@ -134,37 +135,6 @@ export function findFileWithExtension(path: string, files: Map<string, string>):
   }
 
   return null;
-}
-
-/**
- * Calculate the Levenshtein (edit) distance between two strings.
- */
-function levenshtein(a: string, b: string): number {
-  const matrix: number[][] = [];
-
-  for (let i = 0; i <= b.length; i++) {
-    matrix[i] = [i];
-  }
-
-  for (let j = 0; j <= a.length; j++) {
-    matrix[0][j] = j;
-  }
-
-  for (let i = 1; i <= b.length; i++) {
-    for (let j = 1; j <= a.length; j++) {
-      if (b[i - 1] === a[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1];
-      } else {
-        matrix[i][j] = Math.min(
-          matrix[i - 1][j - 1] + 1,
-          matrix[i][j - 1] + 1,
-          matrix[i - 1][j] + 1
-        );
-      }
-    }
-  }
-
-  return matrix[b.length][a.length];
 }
 
 /**
