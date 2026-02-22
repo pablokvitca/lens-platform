514f56a5a0a1be8cb3f292c0c7535066520575fa
Luc Brinkman <75385930+lucbrinkman@users.noreply.github.com>
2026-02-22 18:24:29 +0000

    removed feedback button

--- web_frontend/src/components/GlobalComponents.tsx
+++ web_frontend/src/components/GlobalComponents.tsx
@@ -1,12 +1,10 @@
 import CookieBanner from "./CookieBanner";
-import FeedbackButton from "./FeedbackButton";
 import TosConsentModal from "./TosConsentModal";
 
 export function GlobalComponents() {
   return (
     <>
       <CookieBanner />
-      <FeedbackButton />
       <TosConsentModal />
     </>
   );
